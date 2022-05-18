f23fa0692cf7ddbaf9b161aecfcc86f5df3bb175
AdrianTm1 <awojciechowski1@tm1.edu.pl>
2022-05-18 14:03:04 +0200

    Entities

--- src/main/java/com/example/pizzaaplicationtest/data/entity/PizzaEntity.java
+++ src/main/java/com/example/pizzaaplicationtest/data/entity/PizzaEntity.java
@@ -1,4 +1,30 @@
 package com.example.pizzaaplicationtest.data.entity;
 
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "pizzas")
 public class PizzaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "pizza", orphanRemoval = true)
+    private Set<SizeEntity> sizes;
+
+    public PizzaEntity() {
+    }
+
+    public PizzaEntity(Integer id, String name, Set<SizeEntity> sizes) {
+        this.id = id;
+        this.name = name;
+        this.sizes = sizes;
+    }
 }
--- src/main/java/com/example/pizzaaplicationtest/data/entity/SizeEntity.java
+++ src/main/java/com/example/pizzaaplicationtest/data/entity/SizeEntity.java
@@ -1,4 +1,46 @@
 package com.example.pizzaaplicationtest.data.entity;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+
+@Table(name = "sizes")
 public class SizeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "size_type")
+    private String sizeType;
+
+    @Column(name = "price_base")
+    private BigDecimal priceBase;
+
+    @Column(name = "pizza_id")
+    private Integer pizzaId;
+
+    @OneToMany(mappedBy = "size")
+    private Set<SizeEntity> orderSizes;
+
+    @ManyToOne
+    @JoinColumn(name = "pizza_id", insertable = false, updatable = false)
+    private PizzaEntity pizza;
+
+
+    public SizeEntity() {
+    }
+
+    public SizeEntity(Integer id, String sizeType, BigDecimal priceBase, Integer pizzaId, Set<SizeEntity> orderSizes, PizzaEntity pizza) {
+        this.id = id;
+        this.sizeType = sizeType;
+        this.priceBase = priceBase;
+        this.pizzaId = pizzaId;
+        this.orderSizes = orderSizes;
+        this.pizza = pizza;
+    }
 }
+
